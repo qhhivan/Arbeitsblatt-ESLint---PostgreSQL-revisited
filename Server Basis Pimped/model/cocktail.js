ace9718767cd1be99aec82d4e50db5045b5bfcb7
qhhivan <orsolic.i02@htlwienwest.at>
2021-05-25 21:43:43 +0200

    Fertig

--- Server Basis Pimped/model/cocktail.js
+++ Server Basis Pimped/model/cocktail.js
@@ -13,7 +13,7 @@ async function getAllCocktails() {
 async function getCocktail(name) {
   const { rows } = await db.query(
     'SELECT zbez FROM zutat join besteht on zutat.zid = besteht.zid join cocktail c on besteht.cid = c.cid where cname = $1',
-    [name]
+    [name],
   );
   return {
     code: 200,
@@ -21,5 +21,75 @@ async function getCocktail(name) {
   };
 }
 
+// Aufgabe 6
+async function filterByPrice(price) {
+  const { rows } = await db.query(
+    'SELECT cname, preis from cocktail where preis < $1 order by preis desc',
+    [price],
+  );
+  return {
+    code: 200,
+    data: rows,
+  };
+}
+
+// Aufgabe 7
+async function deleteCocktail(name) {
+  const { rows } = await db.query('SELECT * from cocktail where cname = $1', [
+    name,
+  ]);
+  if (rows.length > 0) {
+    await db.query(
+      'DELETE from besteht where (SELECT cid from cocktail where  cname = $1 ) = cid',
+      [name],
+    );
+    await db.query(
+      'DELETE from bestellt where (SELECT cid from cocktail where  cname = $1) = cid',
+      [name],
+    );
+    await db.query('DELETE from cocktail where cname = $1', [name]);
+
+    return {
+      code: 200,
+      data: `Cocktail ${name} is deleted`,
+    };
+  }
+  return {
+    code: 400,
+    data: `Cocktail ${name} not found `,
+  };
+}
+// Aufgabe 8
+async function createCocktail({ name, preis, zubereitung, kid, zgid, sgid }) {
+  const { rows } = await db.query(
+    'INSERT INTO cocktail (cname, preis, zubereitung, kid, zgid, sgid) values ($1, $2, $3, $4, $5, $6) returning cid;',
+    [name, preis, zubereitung, kid, zgid, sgid],
+  );
+  console.log(rows.cid);
+  return {
+    code: 200,
+    data: `Your new Cocktail has ID ${rows.cid}`,
+  };
+}
+
+// Aufgabe 9
+async function updatePrice(name, preis) {
+  await db.query('UPDATE cocktail set preis = $1 where cname = $2', [
+    preis,
+    name,
+  ]);
+  return {
+    code: 200,
+    data: `New Price: ${preis}`,
+  };
+}
+
 // Export
-module.exports = { getAllCocktails, getCocktail };
+module.exports = {
+  getAllCocktails,
+  getCocktail,
+  filterByPrice,
+  deleteCocktail,
+  createCocktail,
+  updatePrice,
+};
--- Server Basis Pimped/routes/index.js
+++ Server Basis Pimped/routes/index.js
@@ -3,7 +3,14 @@ const asyncHandler = require('express-async-handler');
 
 const router = express.Router();
 
-const { getAllCocktails, getCocktail } = require('../model/cocktail');
+const {
+  getAllCocktails,
+  getCocktail,
+  filterByPrice,
+  deleteCocktail,
+  createCocktail,
+  updatePrice,
+} = require('../model/cocktail');
 
 // Aufgabe 4
 // Implementiere die Route, welche Namen und Preise aller Cocktails zurückliefert
@@ -21,6 +28,52 @@ router.get(
   '/:name/Zutaten',
   asyncHandler(async (req, res) => {
     const result = await getCocktail(req.params.name);
+    res.status(result.code).json(result);
+  }),
+);
+
+// Aufgabe 6
+router.get(
+  '/cocktail/:price',
+  asyncHandler(async (req, res) => {
+    const result = await filterByPrice(req.params.price);
+    res.status(result.code).json(result);
+  }),
+);
+
+router.get(
+  '/cocktail/',
+  asyncHandler(async (req, res) => {
+    const result = await filterByPrice(req.query.price);
+    res.status(result.code).json(result);
+  }),
+);
+
+// Aufgabe 7
+router.delete(
+  '/cocktail/:name',
+  asyncHandler(async (req, res) => {
+    const result = await deleteCocktail(req.params.name);
+    res.status(result.code).json(result);
+  }),
+);
+
+// Aufgabe 8
+// Post ist INSERT
+router.post(
+  '/newCocktail/',
+  asyncHandler(async (req, res) => {
+    const result = await createCocktail(req.body);
+    res.status(result.code).json(result);
+  }),
+);
+
+// Aufgabe 9
+// PATCH ist Update
+router.patch(
+  '/cocktail/:name',
+  asyncHandler(async (req, res) => {
+    const result = await updatePrice(req.params.name, req.body.preis);
     res.status(result.code).json(result);
   }),
 );
